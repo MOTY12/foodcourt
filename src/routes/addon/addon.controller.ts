22caa6c4010f30002657bc82a2632fb4f103fbb7
MOTY <50269513+MOTY12@users.noreply.github.com>
2023-01-09 16:11:08 +0100

    adding role auth

--- src/routes/addon/addon.controller.ts
+++ src/routes/addon/addon.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Request } from "@nestjs/common";
+import { Roles } from "src/decorators/roles.decorator";
 import { AddonService } from "./addon.services";
 import { CreateAddonDto, GetAddonDto, UpdateAddonDto } from "./dto/addon.dto";
 
@@ -10,26 +11,31 @@ export class AddonController {
         private readonly addonService: AddonService
     ){}
 
+    @Roles('admin')
     @Post('/:brandId/addon')
     async createAddon(@Request() request, @Param('brandId') brandId: number, @Body() createAddonDto: CreateAddonDto){
         return this.addonService.createAddon(request.decoded, brandId, createAddonDto);
     }
 
+    @Roles('admin')
     @Get('/:brandId/addon')
     async getAddon( @Param('brandId') brandId: number, @Body() queryString: GetAddonDto){
         return this.addonService.getAddon(brandId, queryString);
     }
 
+    @Roles('admin')
     @Get('/:brandId/addon/:addonId')
     async getAddonById( @Param('brandId') brandId: number, @Param('addonId') addonId: number){
         return this.addonService.getAddonById(brandId, addonId);
     }
 
+    @Roles('admin')
     @Patch('/:brandId/addon/:addonId')
     async updateAddon(@Request() request, @Param('brandId') brandId: number, @Param('addonId') addonId: number, @Body() updateAddonDto: UpdateAddonDto){
         return this.addonService.updateAddon(request.decoded, brandId, addonId, updateAddonDto);
     }
 
+    @Roles('admin')
     @Delete('/:brandId/addon/:addonId')
     async deleteAddon(@Request() request, @Param('brandId') brandId: number, @Param('addonId') addonId: number){
         return this.addonService.deleteAddon(request.decoded, brandId, addonId);
